8271e928b66fba60f4ccb9f5bbf4cea221e1ab0d
Samuel Allemang <samuel.allemang@gmail.com>
2020-03-10 12:23:12 -0400

    Reverted to theme-colours

--- resources/assets/styles/theme-styles.php
+++ resources/assets/styles/theme-styles.php
@@ -1,26 +1,42 @@
-/* stylelint-disable */
+// stylelint-disable
 :root {
-  --site: <?= strtolower( get_field('colour__site', 'option') );?> !important;
-  --primary: <?= strtolower( get_field('colour__primary', 'option') );?> !important;
-  --secondary: <?= strtolower( get_field('colour__secondary', 'option') );?> !important;
-  --info: <?= strtolower( get_field('colour__info', 'option') );?> !important;
-  --text: <?= strtolower( get_field('colour__text', 'option') ) ;?> !important;
-  --background: <?= strtolower( get_field('colour__background', 'option') ) ;?> !important;
+  --site: <?= strtolower( get_field('colour__site', 'option') );?>;
+  --primary: <?= strtolower( get_field('colour__primary', 'option') );?>;
+  --secondary: <?= strtolower( get_field('colour__secondary', 'option') );?>;
+  --info: <?= strtolower( get_field('colour__info', 'option') );?>;
+  --text: <?= strtolower( get_field('colour__text', 'option') ) ;?>;
+  --background: <?= strtolower( get_field('colour__background', 'option') ) ;?>;
 <?php if( get_field('colour__gradient', 'option')['colour__gradient_one'] && get_field('colour__gradient', 'option')['colour__gradient_two'] ): ?>
-  --gradient-one: <?= strtolower( get_field('colour__gradient', 'option')['colour__gradient_one'] ) ;?> !important;
-  --gradient-two: <?= strtolower( get_field('colour__gradient', 'option')['colour__gradient_two'] ) ;?> !important;
+  --gradient-one: <?= strtolower( get_field('colour__gradient', 'option')['colour__gradient_one'] ) ;?>;
+  --gradient-two: <?= strtolower( get_field('colour__gradient', 'option')['colour__gradient_two'] ) ;?>;
 <?php else: ?>
-  --gradient-one: <?= strtolower( get_field('colour__primary', 'option') ); ?> !important;
-  --gradient-two: <?= strtolower( get_field('colour__secondary', 'option') ); ?> !important;
+  --gradient-one: <?= strtolower( get_field('colour__primary', 'option') ); ?>;
+  --gradient-two: <?= strtolower( get_field('colour__secondary', 'option') ); ?>;
 <?php endif; ?>
 }
+
+$theme-colors: (
+  site: <?= strtolower( get_field('colour__site', 'option') );?>,
+  primary: <?= strtolower( get_field('colour__primary', 'option') );?>,
+  secondary: <?= strtolower( get_field('colour__secondary', 'option') );?>,
+  info: <?= strtolower( get_field('colour__info', 'option') );?>,
+  text: <?= strtolower( get_field('colour__text', 'option') );?>,
+  background: <?= strtolower( get_field('colour__background', 'option') );?>,
+<?php if( get_field('colour__gradient', 'option')['colour__gradient_one'] && get_field('colour__gradient', 'option')['colour__gradient_two'] ): ?>
+  gradient-one: <?= strtolower( get_field('colour__gradient', 'option')['colour__gradient_one'] );?>,
+  gradient-two: <?= strtolower( get_field('colour__gradient', 'option')['colour__gradient_two'] );?>
+<?php else: ?>
+  gradient-one: <?= strtolower( get_field('colour__primary', 'option') ); ?>,
+  gradient-two: <?= strtolower( get_field('colour__secondary', 'option') ); ?>
+<?php endif; ?>
+);
 
 h1,
 .h1 {
 <?php if( get_field('colour__typography', 'option')['colour__h1'] ): ?>
   color: <?= strtolower( get_field('colour__typography', 'option')['colour__h1'] ); ?>;
 <?php else: ?>
-  color: var(--primary);
+  color: theme-color("primary");
 <?php endif; ?>
 }
 
@@ -29,7 +45,7 @@ h2,
 <?php if( get_field('colour__typography', 'option')['colour__h2'] ): ?>
   color: <?= strtolower( get_field('colour__typography', 'option')['colour__h2'] ); ?>;
 <?php else: ?>
-  color: var(--secondary);
+  color: theme-color("secondary");
 <?php endif; ?>
 }
 
@@ -38,7 +54,7 @@ h3,
 <?php if( get_field('colour__typography', 'option')['colour__h3'] ): ?>
   color: <?= strtolower( get_field('colour__typography', 'option')['colour__h3'] ); ?>;
 <?php else: ?>
-  color: var(--info);
+  color: theme-color("info");
 <?php endif; ?>
 }
 
@@ -47,72 +63,67 @@ h4,
 <?php if( get_field('colour__typography', 'option')['colour__h4'] ): ?>
   color: <?= strtolower( get_field('colour__typography', 'option')['colour__h4'] ); ?>;
 <?php else: ?>
-  color: var(--secondary);
+  color: theme-color("secondary");
 <?php endif; ?>
 }
 
-section.block-cta h2.line-bottom {
+section.block-cta {
+  h2 {
+    &.line-bottom {
 <?php if( get_field('colour__typography', 'option')['colour__cta'] ): ?>
-  color: <?= strtolower( get_field('colour__typography', 'option')['colour__cta'] ); ?>;
-  border-bottom: 4px solid <?= strtolower( get_field('colour__typography', 'option')['colour__cta'] ); ?>;
+      color: <?= strtolower( get_field('colour__typography', 'option')['colour__cta'] ); ?>;
+      border-bottom: 4px solid <?= strtolower( get_field('colour__typography', 'option')['colour__cta'] ); ?>;
 <?php else: ?>
-  color: var(--primary);
-  border-bottom: 4px solid var(--primary);
+      color: theme-color("primary");
+      border-bottom: 4px solid theme-color("primary");
 <?php endif; ?>
-}
+    }
+  }
 
-section.block-cta h3 {
+  h3 {
 <?php if( get_field('colour__typography', 'option')['colour__cta'] ): ?>
-  color: <?= strtolower( get_field('colour__typography', 'option')['colour__cta'] ); ?>;
+    color: <?= strtolower( get_field('colour__typography', 'option')['colour__cta'] ); ?>;
 <?php else: ?>
-  color: var(--info);
+    color: theme-color("info");
 <?php endif; ?>
+  }
 }
 
-<?php if( get_field('branding__popup_background', 'option')): ?>
 .icegram.ig_popup,
 .ig_popup.ig_inspire.ig_popup {
+<?php if( get_field('branding__popup_background', 'option')): ?>
   background: url("<?= App\force_relative_url( get_field('branding__popup_background', 'option')['sizes']['large'] ); ?>");
   position: relative;
   background-size: cover;
-}
 
-.icegram.ig_popup::before,
-.ig_popup.ig_inspire.ig_popup::before {
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  content: "";
-  top: 0;
-  left: 0;
-  display: block;
-  z-index: 0;
-  background-color: var(--background);
-  opacity: 0.8;
-}
+  &::before {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    content: "";
+    top: 0;
+    left: 0;
+    display: block;
+    z-index: 0;
+    background-color: rgba(theme-color("background"), 0.8);
+  }
 
-.icegram.ig_popup .ig_headline,
-.ig_popup.ig_inspire.ig_popup .ig_headline {
-  color: white;
-}
-
-
+  .ig_headline {
+    color: white;
+  }
 <?php else: ?>
-.icegram.ig_popup,
-.ig_popup.ig_inspire.ig_popup {
-  background-color: var(--primary) !important;
+  background-color: theme-color("primary") !important;
   color: white;
-}
 
-.icegram.ig_popup .ig_headline,
-.ig_popup.ig_inspire.ig_popup .ig_headline {
-  color: white;
-}
+  .ig_headline {
+    color: white;
+  }
 <?php endif; ?>
+}
 
 input,
 textarea {
-<?php if( get_field('colour__forms', 'option')['colour__form_inputs']): ?>
+  <?php if( get_field('colour__forms', 'option')['colour__form_inputs']): ?>
   background-color: <?= strtolower( get_field('colour__forms', 'option')['colour__form_inputs']) ;?> !important;
   border-color: <?= strtolower( get_field('colour__forms', 'option')['colour__form_inputs'] ) ;?>  !important;
   <?php else: ?>
@@ -130,4 +141,5 @@ label,
   <?php endif; ?>
 }
 
-/* stylelint-enable */
+
+// stylelint-enable
